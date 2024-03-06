af0ef726b79f91ea28279bd01b71a5195e2fa4c0
Enzo Afif Olegário Fattah <enzotope9@gmail.com>
2024-03-06 10:46:12 -0300

    first commit

---
+++ Atividade2-POO/Aluno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2_POO
+{
+    public class Aluno
+    {
+        public string RA;
+        public string Nome;
+        public decimal NotaProva;
+        public decimal NotaTrabalho;
+        public decimal NotaFinal;
+
+        public void ReceberDados()
+        {
+            Console.Write("Digite o RA do aluno: ");
+            RA = Console.ReadLine();
+
+            Console.Write("Digite o nome do aluno: ");
+            Nome = Console.ReadLine();
+
+            Console.Write("Digite a nota da prova: ");
+            NotaProva = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota do trabalho: ");
+            NotaTrabalho = decimal.Parse(Console.ReadLine());
+        }
+
+        public void CalcularMedia()
+        {
+            NotaFinal = (NotaProva + NotaTrabalho) / 2;
+        }
+        public void CalcularNotaFinal()
+        {
+            if(NotaFinal >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+        public void ImprimirNotaFinal()
+        {
+            Console.WriteLine("A nota final é: " + NotaFinal);
+        }
+    }
+}
---
+++ Atividade2-POO/Program.cs
@@ -0,0 +1,8 @@
+﻿using Atividade2_POO;
+
+Aluno aluno = new();
+
+aluno.ReceberDados();
+aluno.CalcularMedia();
+aluno.CalcularNotaFinal();
+aluno.ImprimirNotaFinal();
